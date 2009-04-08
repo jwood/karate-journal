d7272c0a1d99ec37b533ade55fec8ba3147669e5
John Wood <john@interactivemediums.com>
2009-04-08 08:20:29 -0500

    Some cleanup

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,29 +2,28 @@ class Entry < ActiveRecord::Base
   has_one :entry_type
 
   def body_without_fragment_links
-    body.gsub(/<lfrag [^>]+>/, "").
-         gsub("</lfrag>", "").
-         gsub(/<bfrag [^>]+>/, "").
-         gsub("</bfrag>", "")
+    body.gsub(/<lfrag [^>]+>/, "").gsub("</lfrag>", "").
+         gsub(/<bfrag [^>]+>/, "").gsub("</bfrag>", "")
   end
 
   def line_fragments
-    frags = []
-    Entry.find(:all, :conditions => "body like '%#{self.title}%'").each do |entry|
-      entry.body =~ /<lfrag [^>]+#{self.title}[^>]*>(.*)<\/lfrag>/
-      fragment = $1.sub(/<\/lfrag>.*/, "").strip
-      frags << {:source => entry, :fragment => fragment}
-    end
-    frags
+    fragments("lfrag")
   end
 
   def body_fragments
+    fragments("bfrag")
+  end
+
+  private
+
+  def fragments(fragment_type)
     frags = []
     Entry.find(:all, :conditions => "body like '%#{self.title}%'").each do |entry|
-      entry.body =~ /<bfrag [^>]+#{self.title}[^>]*>(.*)<\/bfrag>/
-      fragment = $1.sub(/<\/bfrag>.*/, "").strip
+      entry.body =~ /<#{fragment_type} [^>]+#{self.title}[^>]*>(.*)<\/#{fragment_type}>/
+      fragment = $1.sub(/<\/#{fragment_type}>.*/, "").strip
       frags << {:source => entry, :fragment => fragment}
     end
     frags
   end
+  
 end
